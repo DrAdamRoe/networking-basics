12271fa86498084f002e2b6746a797b9898ca087
Adam Roe <adam.j.roe@gmail.com>
2018-11-02 11:23:49 +0100

    document and clarify code

--- sockets-intro/client.py
+++ sockets-intro/client.py
@@ -1,14 +1,15 @@
 ### adapted from https://github.com/realpython/materials/tree/master/python-sockets-tutorial
 
-
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, this is the client")
-    data = s.recv(1024)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as aSocket:
+    aSocket.connect((HOST, PORT)) # connect the socket to a host and port
+    msg = "Hello, this is the client"
+    print("sending to the server: ", msg)
+    aSocket.sendall(msg.encode()) # send your message with utf-8 encoding
+    data = aSocket.recv(1024) # wait for a response
 
-print("Received", repr(data))
+print("Received from server: ", data.decode()) #print response
--- sockets-intro/server.py
+++ sockets-intro/server.py
@@ -1,19 +1,25 @@
 ### adapted from https://github.com/realpython/materials/tree/master/python-sockets-tutorial
+## documentation at https://docs.python.org/3/library/socket.html
+## more good info at https://docs.python.org/3/howto/sockets.html
 
 import socket
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print("Connected by", addr)
+# AF_INET: use an IPv4 address
+# SOCK_STREAM: use a stream socket, e.g. TCP
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as aSocket:
+    aSocket.bind((HOST, PORT))
+    aSocket.listen()
+    print("server started at address %s on port %i" % (HOST, PORT))
+    connection, address = aSocket.accept() ## accept incoming connection
+    with connection:
+        print("Client connected from: ", address)
         while True:
-            data = conn.recv(1024)
+            data = connection.recv(1024) ## receive byte data from the socket
             if not data:
                 break
-            print(data)
-            conn.sendall(data)
+            print("message from client: ", data.decode())
+            msg_response = "server got your data: " + data.decode()
+            connection.sendall(msg_response.encode())
