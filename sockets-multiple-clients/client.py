8f16d1225b5780dc4b5d7f490e0ec4a9631f53b6
Athi Fongoqa <athi.fongoqa@code.berlin>
2021-03-13 16:57:19 +0200

    Multiple clients communication

---
+++ sockets-multiple-clients/client.py
@@ -0,0 +1,32 @@
+import socket
+
+HEADER = 64  # 64 bytes
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "10.0.0.4"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+send("Hello World!")
+input()
+send("Hello World!")
+send("Hello World!")
+input()
+send("Hello World!")
+send("Hello World!")
+input()
+send(DISCONNECT_MESSAGE)
---
+++ sockets-multiple-clients/server.py
@@ -0,0 +1,62 @@
+import socket
+import threading
+
+# 1
+HEADER = 64  # 64 bytes
+PORT = 5050
+# socket.gethostbyname(socket.gethostname()) will get the IP address automatically for you
+# To connect to the internet, change the value to your public IP address
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+
+# 2.    Open up socket to connect to other devices
+#       Bind socket to port and IP address
+#       AF_INET tells the socket that it must accept an IPv4 address
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+# 5.    To send messages between clients, store the indiv messages globally & run new thread
+
+
+# 4.    Handles indiv communication between client and server
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        # convert from bytes to string using utf-8
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:  # blocking line
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+
+    conn.close()
+
+
+# 3.    This function will allow our server to:
+#           listen to connections
+#           handle them
+#           pass them onto handle_client which will run in new thread
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        # Waits for new connection to server
+        # When new connection occurs:
+        #       it will store the IP and port as addr
+        #       store socket object (as conn) which allows us send info back
+        conn, addr = server.accept()  # blocking line
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount()- 1}")
+
+
+print("[STARTING] server is starting...")
+start()
