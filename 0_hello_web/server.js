31bfe9454e0f85a639cf696d66dabcbf0d699e7d
Adam Roe <adam.j.roe@gmail.com>
2023-07-13 10:45:49 +0200

    add first js client, modify server appropriately

--- 0_hello_web/server.js
+++ 0_hello_web/server.js
@@ -2,7 +2,8 @@ const net = require("net");
 const server = net.createServer();
 
 server.on("connection", handleConnection);
-server.listen(3000);
+server.listen({ port: 3000 });
+console.log("Server is ready! Listening on port 3000…")
 
 function handleConnection(socket) {
   socket.on("data", (chunk) => {
